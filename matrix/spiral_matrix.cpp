44cc4affeac75708b13f52c81e5758408b66a529
saimon-35 <zia73575@gmail.com>
2025-01-22 01:34:33 +0600

    Refactor boundary checks in spiral matrix traversal implementation

--- matrix/spiral_matrix.cpp
+++ matrix/spiral_matrix.cpp
@@ -7,16 +7,16 @@ public:
     int a = 0, b = matrix.size() - 1;
     int c = 0, d = matrix[0].size() - 1;
     
-    while(a <= b && c <= d){
+    while(a <= b && c <= d){//a,b,c,d are the boundaries of the matrix
         for(int i = a; i <= d; i++) ans.push_back(matrix[a][i]); //right
         a++;
         for(int j = a; j <= b; j++) ans.push_back(matrix[j][d]); //down
         d--;
-        if(a <= b){
+        if(a <= b){//check if the row still exists
             for(int i = d; i >= c; i--) ans.push_back(matrix[b][i]); //left
             b--;
         }
-        if(c <= d){
+        if(c <= d){//check if the column still exists
             for(int j = b; j >= a; j--) ans.push_back(matrix[j][c]);
         }
         c++;
